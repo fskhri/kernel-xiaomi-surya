a09a75a232fbbc1bb1d316258b2983d99a8206e9
Fakhri Djuanda <41115223+fskhri@users.noreply.github.com>
2024-06-24 06:49:42 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,38 +1,44 @@
 #!/usr/bin/env bash
+
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/fskhri/kernel-xiaomi-surya -b  sbv6  kernel
+git clone --depth=1 https://github.com/fskhri/kernel-xiaomi-surya -b sbv6 kernel
 cd kernel
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b lld-integration gcc64
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b lld-integration gcc32
+git clone --depth=1 https://github.com/llvm/llvm-project llvm
 git clone --depth=1 https://github.com/fskhri/AnyKernel3 -b ribka AnyKernel
 git clone --depth=1 https://android.googlesource.com/platform/system/libufdt libufdt
 echo "Done"
+
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 LOG=$(echo *.log)
 START=$(date +"%s")
+
 export CONFIG_PATH=$PWD/arch/arm64/configs/surya-perf_defconfig
 TC_DIR=${PWD}
-GCC64_DIR="${PWD}/gcc64"
-GCC32_DIR="${PWD}/gcc32"
-PATH="$TC_DIR/bin/:$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH"
+LLVM_DIR="${PWD}/llvm"
+PATH="${LLVM_DIR}/bin:/usr/bin:$PATH"
+
 export ARCH=arm64
 export KBUILD_BUILD_HOST="LuLu"
 export KBUILD_BUILD_USER="Ribka"
+export CLANG_TRIPLE=aarch64-linux-gnu-
+
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$BOTTOKEN/sendSticker" \
         -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
         -d chat_id=$CHATID
 }
+
 # Send info plox channel
 function sendinfo() {
     curl -s -X POST "https://api.telegram.org/bot$BOTTOKEN/sendMessage" \
         -d chat_id="$CHATID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• surya-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Poco X3</b> (picasso)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Proton clang 12</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #AOSP-Alpha"
+        -d text="<b>• surya-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Poco X3</b> (picasso)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Clang</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #AOSP-Alpha"
 }
+
 # Push kernel to channel
 function push() {
     cd AnyKernel
@@ -41,8 +47,9 @@ function push() {
         -F chat_id="$CHATID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco X3 (surya)</b> | <b>Eva GCC</b>"
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco X3 (surya)</b> | <b>Eva Clang</b>"
 }
+
 # Fin Error
 function finerr() {
     curl -F document=@$LOG "https://api.telegram.org/bot$BOTTOKEN/sendDocument" \
@@ -51,27 +58,32 @@ function finerr() {
         -F "parse_mode=html" \
         -F caption="Build logs"
 }
+
 # Compile plox
 function compile() {
-   make O=out ARCH=arm64 surya-perf_defconfig
-       make -j$(nproc --all) O=out \
-                             ARCH=arm64 \
-			     CROSS_COMPILE_ARM32=arm-eabi- \
-			     CROSS_COMPILE=aarch64-elf- \
-			     AR=aarch64-elf-ar \
-			     OBJDUMP=aarch64-elf-objdump \
-			     STRIP=aarch64-elf-strip 2>&1 | tee error.log
-   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-   python2 "libufdt/utils/src/mkdtboimg.py" \
-					create "out/arch/arm64/boot/dtbo.img" --page_size=4096 out/arch/arm64/boot/dts/qcom/*.dtbo
-   cp out/arch/arm64/boot/dtbo.img AnyKernel
+    make O=out ARCH=arm64 surya-perf_defconfig
+    make -j$(nproc --all) O=out \
+                          ARCH=arm64 \
+                          CC=clang \
+                          CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE=aarch64-linux-gnu- \
+                          CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                          AR=llvm-ar \
+                          OBJDUMP=llvm-objdump \
+                          STRIP=llvm-strip 2>&1 | tee error.log
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+    python2 "libufdt/utils/src/mkdtboimg.py" \
+            create "out/arch/arm64/boot/dtbo.img" --page_size=4096 out/arch/arm64/boot/dts/qcom/*.dtbo
+    cp out/arch/arm64/boot/dtbo.img AnyKernel
 }
+
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
     zip -r9 surya-Stormbreaker-${TANGGAL}.zip *
     cd ..
 }
+
 sticker
 sendinfo
 compile
